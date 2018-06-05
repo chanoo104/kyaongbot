a68c68973ef64e9f4e67163f2210bb4fe2ac6220
K'rome <37375701+sealmin@users.noreply.github.com>
2018-06-06 01:15:07 +0900

    Update response.js

--- response.js
+++ response.js
@@ -1,7 +1,7 @@
 /* [KyaongBot] */
 var timeo = new Date().getTime();
-var ver = "4.5.7.0_f"
-var updatecode = "!8ball "
+var ver = "4.5.6.0_s"
+var updatecode = "!명령어 "
 var error = false;
 var errorchk = 0;
 
@@ -873,35 +873,35 @@ if (msg.indexOf("8ball ") == 0) {
     if (msg.indexOf("?") == 0) {
         var e_ball_rand = generateRandom(1, 16);
         if (e_ball_rand == 1){
-            replier.reply("전망이 좋지 않습니다..")
+            replier.reply("전망이 좋지 않습니다..");
         } else if (e_ball_rand == 2){
-            replier.reply("아니요!")
+            replier.reply("아니요!");
         } else if (e_ball_rand == 3){
-            replier.reply("제 직감으로 봐서는 아닐겁니다!")
+            replier.reply("제 직감으로 봐서는 아닐겁니다!");
         } else if (e_ball_rand == 4){
-            replier.reply("제 답변은...아니오입니다!")
+            replier.reply("제 답변은...아니오입니다!");
         } else if (e_ball_rand == 5){
-            replier.reply("그렇지 않을겁니다..")
+            replier.reply("그렇지 않을겁니다..");
         } else if (e_ball_rand == 6){
-            replier.reply("앗..! 깜빡하고 졸아서 질문을 받지 못하였습니다..")
+            replier.reply("앗..! 깜빡하고 졸아서 질문을 받지 못하였습니다..");
         } else if (e_ball_rand == 7){
-            replier.reply("아앗..집중해서 다시 해보세요!")
+            replier.reply("아앗..집중해서 다시 해보세요!");
         } else if (e_ball_rand == 8){
-            replier.reply("예측을..못하겠습니다..!")
+            replier.reply("예측을..못하겠습니다..!");
         } else if (e_ball_rand == 9){
-            replier.reply("아마.. 그렇지 않을까요?")
+            replier.reply("아마.. 그렇지 않을까요?");
         } else if (e_ball_rand == 10){
-            replier.reply("아마도 그럴겁니다!")
+            replier.reply("아마도 그럴겁니다!");
         } else if (e_ball_rand == 11){
-            replier.reply("저를 믿어도 좋습니다!")
+            replier.reply("저를 믿어도 좋습니다!");
         } else if (e_ball_rand == 12){
-            replier.reply("네!")
+            replier.reply("네!");
         } else if (e_ball_rand == 13){
-            replier.reply("의심할 여지 없이 당연합니다!")
+            replier.reply("의심할 여지 없이 당연합니다!");
         } else if (e_ball_rand == 14){
-            replier.reply("확실히 그렇습니다!")
+            replier.reply("확실히 그렇습니다!");
         } else if (e_ball_rand == 15){
-            replier.reply("당연히 그럴겁니다!")
+            replier.reply("당연히 그럴겁니다!");
         }          
     }
 }
