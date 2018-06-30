81275ac2510115c3c1d8142ecf8758c7bdbe7506
rgb-odosk <38985060+rgb-odosk@users.noreply.github.com>
2018-06-30 20:42:36 +0900

    Update response.js

--- response.js
+++ response.js
@@ -753,7 +753,7 @@ loop: {
     var list = new Array();
     list.push("[검색결과]");
     for (var i = 0; i < DB.icode.length; i++) {
-        if (DB.inick[i].toLowerCase().indexOf(key.toLowerCase()) =! -1) {
+        if (DB.inick[i].toLowerCase().indexOf(key.toLowerCase()) != -1) {
         list.push("\n" + DB.inick[i] + " - " + DB.icode[i]);
     }
     }
