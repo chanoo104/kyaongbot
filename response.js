19e25c72911572d03b4be068989c83f8c39c79ca
AMD TR™ <16631452+AMD-TR@users.noreply.github.com>
2018-07-04 00:56:50 +0900

    Update response.js

--- response.js
+++ response.js
@@ -48,7 +48,10 @@ UPDATE.saveData = function(msg) { //파일에 내용을 저장하는 함수
 };
 
 function isInt(x) {
+	if (x.indexOf(".") == -1) {
     return !isNaN(x) && eval(x).toString().length == parseInt(eval(x)).toString().length
+    } else {
+    	return false
 }
 
 function getHtml(text) {
