3a75daba86ab465156cbed1d36d841c689af0655
rgb-odosk <38985060+rgb-odosk@users.noreply.github.com>
2018-07-07 18:36:02 +0900

    Update response.js

--- response.js
+++ response.js
@@ -1044,7 +1044,7 @@ if (msg.split(" ")[0] == "!복권") {
 
 if (msg.split(" ")[0] == "!가위" || msg.split(" ")[0] == "!바위" || msg.split(" ")[0] == "!보") {
 if (new Date().getHours() == 18 || new Date().getHours() == 0 || new Date().getHours() == 7) {
-if (new Date().getMinutes()>=30 && new Date().getMinutes()<=59){
+if (new Date().getMinutes() >= 30){
 // 가위바위보
 loop: {
 if (msg.split(" ")[0] == "!가위") {
