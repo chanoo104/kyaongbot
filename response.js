18ace6be0570394d9a73f386fda68e50db4b83e3
chanoo104 <chanoo104@naver.com>
2018-07-30 15:25:24 +0900

    수정

--- response.js
+++ response.js
@@ -148,7 +148,7 @@ if (room == "불여우") {
 // 프사 저장
     if (DataBase.getDataBase(sender + "profile") == undefined) {
         DataBase.setDataBase(sender + "profile", java.lang.String(ImageDB.getProfileImage()).hashCode())
-        replier.reply("[" + sender + "]님의 프로필을 새로 DB에 추가했습니다.\n" + java.lang.String(ImageDB.getProfileImage()).hashCode())
+        replier.reply("[" + sender + "]님의 프로필 사진을 새로 DB에 추가했습니다.\n" + java.lang.String(ImageDB.getProfileImage()).hashCode())
     }
 // 프사 확인
     if (msg == "!프사확인") {
@@ -157,7 +157,7 @@ if (room == "불여우") {
 // 프사 변경시 새로 추가
     if (DataBase.getDataBase(sender + "profile") != java.lang.String(ImageDB.getProfileImage()).hashCode()) {
         DataBase.setDataBase(sender + "profile", java.lang.String(ImageDB.getProfileImage()).hashCode())
-        replier.reply("[" + sender + "]님의 프사가 변경된것 같아 DB에 수정했습니다.\n" + java.lang.String(ImageDB.getProfileImage()).hashCode())
+        replier.reply("[" + sender + "]님의 프로필 사진이 변경된것 같아 DB에 수정했습니다.\n" + java.lang.String(ImageDB.getProfileImage()).hashCode())
     }
 /////////////////////////////////////////////////////////////////
 /*
