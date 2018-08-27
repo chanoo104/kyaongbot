8a3ea6912b7f07162d276656c1b15d254c592edf
chanoo104 <chanoo104@naver.com>
2018-08-27 16:12:59 +0900

    ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ

--- response.js
+++ response.js
@@ -263,10 +263,14 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB) {
                 DB.p[scode].callmsg = new Array()
                 DB.p[scode].lottery = false
                 DB.p[scode].pns = 0
-                DB.p[scode].realwr = 0
+                
+                
 
 
 
+            }
+            if (DB.p[scode].realwr == undefined){
+                DB.p[scode].realwr == 0
             }
 
 
