435659cf1f862338cb953a7f75099d34d90bde7d
chanoo104 <chanoo104@naver.com>
2018-08-31 17:57:32 +0900

    dddddddddddddddddddddddddddddddddddddddddd

--- response.js
+++ response.js
@@ -1400,11 +1400,12 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB) {
             try {
                 if (msg.indexOf("!롤전적") == 0) {
                     msgi = msg.replace(/ /g, "+"); //메세지 부분에 공백부분을 +로 대체해줍니다 (그냥 띄어쓰기용)
-                    var u = Utils.getWebText("http://www.op.gg/summoner/userName=" + msgi.substr(4)); //변수 u는 이링크를 HTML파싱한 값이다
-                    docc = org.jsoup.Jsoup.connect("http://www.op.gg/summoner/userName=" + msgi.substr(4)).header("Accept-Language", "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7").get()
                     if (u.split('<span class="tierRank">')[1].split("</span>")[0] == "Unranked"){
+                        docc = org.jsoup.Jsoup.connect("http://www.op.gg/summoner/userName=" + msgi.substr(4)).header("Accept-Language", "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7").get()
                         replier.reply("[" + msg.substr(5) + "]\n티어 : 언랭\n레벨 : " + docc.select(".ProfileIcon").select(".level").text())
                     }else{
+                        docc = org.jsoup.Jsoup.connect("http://www.op.gg/summoner/userName=" + msgi.substr(4)).header("Accept-Language", "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7").get()
+                        var u = Utils.getWebText("http://www.op.gg/summoner/userName=" + msgi.substr(4)); //변수 u는 이링크를 HTML파싱한 값이다
                     var t = u.split("<span class=\"tierRank\">"); //변수 a는 변수 u에서 HTML에 <span class="tierRank"> 을 자른값 입니다 /이걸로 해서 tierRank부분을 자른겁니다
                     var w = u.split("<span class=\"wins\">"); //나머지도 마찬가지입니다
                     var l = u.split("<span class=\"losses\">");
