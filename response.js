eae3297581576a3fb503dc702751e37dab2cc9d4
chanoo104 <chanoo104@naver.com>
2018-09-07 00:39:59 +0900

    웅기이이이잇

--- response.js
+++ response.js
@@ -268,6 +268,7 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB) {
                     DataBase.setDataBase(sender + "once", "true")
                     return;
                 }
+                else return;
             }
         }
         // 프사 변경시 새로 추가
@@ -1049,30 +1050,30 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB) {
                             if (DB.p[scode].realwr == "undefined") {
                                 DB.p[scode].realwr == Number(0)
                             }
-                            if (DB.p[scode].realwr == "NaN") { 
-                                DB.p[scode].realwr == Number(0) 
-                            } 
-                            DB.p[scode].realwr++; 
+                            if (DB.p[scode].realwr == "NaN") {
+                                DB.p[scode].realwr == Number(0)
+                            }
+                            DB.p[scode].realwr++;
                             replier.reply("[강퇴 경고 추가]\n[" + sender + DB.p[scode].realwr + "회]\n욕설 횟수가 10회를 초과하여 강퇴경고가 1회 추가되었습니다. 앞으로 5번 더쓰시면 강퇴경고가 한번더 들어갑니다.\n경고 횟수가 3회가 되면 강퇴가 되니 주의 하십시오.")
                         }
                         if (DB.p[scode].warning == 15) {
                             if (DB.p[scode].realwr == "undefined") {
                                 DB.p[scode].realwr == Number(0)
                             }
-                            if (DB.p[scode].realwr == "NaN") { 
-                                DB.p[scode].realwr == Number(0) 
-                            } 
-                            DB.p[scode].realwr++; 
+                            if (DB.p[scode].realwr == "NaN") {
+                                DB.p[scode].realwr == Number(0)
+                            }
+                            DB.p[scode].realwr++;
                             replier.reply("[강퇴 경고 추가]\n[" + sender + DB.p[scode].realwr + "회]\n욕설 횟수가 15회를 초과하여 강퇴경고가 1회 추가되었습니다. 앞으로 5번 더쓰시면 강퇴경고가 한번더 들어갑니다.\n경고 횟수가 3회가 되면 강퇴가 되니 주의 하십시오.")
                         }
                         if (DB.p[scode].warning == 20) {
                             if (DB.p[scode].realwr == "undefined") {
                                 DB.p[scode].realwr == Number(0)
                             }
-                            if (DB.p[scode].realwr == "NaN") { 
-                                DB.p[scode].realwr == Number(0) 
-                            } 
-                            DB.p[scode].realwr++; 
+                            if (DB.p[scode].realwr == "NaN") {
+                                DB.p[scode].realwr == Number(0)
+                            }
+                            DB.p[scode].realwr++;
                             replier.reply("[강퇴 경고 추가]\n[" + sender + DB.p[scode].realwr + "회]\n욕설 횟수를 20회 초과 하셨습니다. " + sender + "님은 강퇴처리 됩니다.")
                         }
                         if (DB.p[scode].warning >= 10) {
