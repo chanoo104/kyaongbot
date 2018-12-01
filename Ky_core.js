9e56ca4b95a5b52321f14938dce76dea593cfaad
chanoo104 <chanoo104@naver.com>
2018-12-01 20:16:47 +0900

    ㅇㅇㅇ

--- Ky_core.js
+++ Ky_core.js
@@ -1460,7 +1460,7 @@ function miscSys(params) {
 		d = '컴퓨터를 제대로 활용 못하는 이를 위해.';
 		if (commandChk(params, c, a, d) == false) break loop;
 		if (msg.substr(0, c.length + 1) == c + ' ') {
-			replier.reply(shortURL('http://ko.lmgtfy.com/?t=i&q=' + encodeURI(msg.substring(c.length + 1))))
+			replier.reply(shortURL('http://ko.lmgtfy.com/?q=' + encodeURI(msg.substring(c.length + 1))))
 			replier.reply("전체보기 클릭" + blank + "1. 브라우저를 켜고 구글(google.co.kr)에 들어가세요.\n2.궁금한 내용(" + msg.substring(c.length + 1)+ ")을 입력하세요.\n3.검색 버튼을 누르세요.\n\n참 쉽죠?")
 		}
 	}
